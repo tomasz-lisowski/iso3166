60b98cb893a923d072c2268db7aa03e0cea48d16
Tomasz Lisowski <t.p.lisowski@gmail.com>
2024-01-11 11:49:48 +0000

    Update to Zig 0.12.0-dev.2127+fcc0c5ddc

--- build.zig
+++ build.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("iso3166", .{
-        .source_file = .{ .path = "iso3166.zig" },
-        .dependencies = &[_]std.Build.ModuleDependency{},
+        .root_source_file = .{ .path = "iso3166.zig" },
     });
 }
