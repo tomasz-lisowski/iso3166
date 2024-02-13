64c8ad809412ffe721898da1286b0b05bed8eb6e
Tomasz Lisowski <t.p.lisowski@gmail.com>
2024-02-13 13:49:18 +0000

    Add package description and update README

--- build.zig
+++ build.zig
@@ -1,19 +1,30 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.addStaticLibrary(.{
+        .name = "iso3166",
+        .root_source_file = .{ .path = "iso3166.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    b.installArtifact(lib);
 
     _ = b.addModule("iso3166", .{
         .root_source_file = .{ .path = "iso3166.zig" },
     });
 
-    const unit_tests = b.addTest(.{
+    const lib_unit_tests = b.addTest(.{
         .root_source_file = .{ .path = "iso3166.zig" },
         .target = target,
         .optimize = optimize,
     });
-    const run_unit_tests = b.addRunArtifact(unit_tests);
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
+    test_step.dependOn(&run_lib_unit_tests.step);
 }
