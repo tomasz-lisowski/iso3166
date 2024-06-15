e17a681178cd990b87e9f30e5af6bb54ab4dfd49
Tomasz Lisowski <t.p.lisowski@gmail.com>
2024-06-15 16:58:18 +0100

    Zig 0.14.0-dev.32+4aa15440c

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) void {
 
     const lib = b.addStaticLibrary(.{
         .name = "iso3166",
-        .root_source_file = .{ .path = "iso3166.zig" },
+        .root_source_file = b.path("iso3166.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -14,11 +14,11 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(lib);
 
     _ = b.addModule("iso3166", .{
-        .root_source_file = .{ .path = "iso3166.zig" },
+        .root_source_file = b.path("iso3166.zig"),
     });
 
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "iso3166.zig" },
+        .root_source_file = b.path("iso3166.zig"),
         .target = target,
         .optimize = optimize,
     });
