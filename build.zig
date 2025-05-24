ea2484dbe21b23379a8691e60fae5f3d5416d9a7
Tomasz Lisowski <t.p.lisowski@gmail.com>
2025-05-24 17:00:23 +0100

    Update to Zig 0.15.0-dev.631+9a3540d61

--- build.zig
+++ build.zig
@@ -4,25 +4,26 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "iso3166",
-        .root_source_file = b.path("iso3166.zig"),
+    const lib_mod = b.createModule(.{
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "iso3166",
+        .root_module = lib_mod,
+    });
     b.installArtifact(lib);
 
     _ = b.addModule("iso3166", .{
-        .root_source_file = b.path("iso3166.zig"),
+        .root_source_file = b.path("src/root.zig"),
     });
 
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("iso3166.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = lib_mod,
     });
-
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
